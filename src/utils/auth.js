7ccef2bca86d04dd310e0c7a707453934836adf0
Marco Baiad <marcobaiad@gmail.com>
2020-10-23 17:11:42 -0300

    remove setitems id, role

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,14 +4,14 @@ export default {
     },
     logedIn: (token, role, id) => {
         localStorage.setItem('token', token);
-        localStorage.setItem('role', role);
-        localStorage.setItem('id', id);
+        // localStorage.setItem('role', role);
+        // localStorage.setItem('id', id);
     },
     logOut: () => {
         localStorage.removeItem('token');
-        localStorage.removeItem('role');
-        localStorage.removeItem('id');
-        localStorage.removeItem('username');
+        // localStorage.removeItem('role');
+        // localStorage.removeItem('id');
+        // localStorage.removeItem('username');
     },
 
     hasRole: role => {
