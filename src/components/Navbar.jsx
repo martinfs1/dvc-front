7acf55da346b6cfd84377f00f489dcd334fda302
Marco Baiad <marcobaiad@gmail.com>
2020-11-16 19:36:54 -0300

    add gh pages agaiiiin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Administracion() {
         try {
             await clienteAxios.get(`/api/v1/logout`);
             auth.logOut();
-            window.location = '/';
+            window.location = '/dvc';
         } catch (e) {
             const { response } = e;
             if (response.data.error && response.data.error.includes('expired')) {
