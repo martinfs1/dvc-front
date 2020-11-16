a5aa87f67b0cfb0aeef134d1198d331475d601b5
Marco Baiad <marcobaiad@gmail.com>
2020-11-16 18:27:15 -0300

    Updates

---
+++ precache-manifest.4ca762f099709688bee9614396a469b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac1ebd864a0724f10a8681662675d8",
+    "url": "/dvc/index.html"
+  },
+  {
+    "revision": "14df406433e67e41ef80",
+    "url": "/dvc/static/css/main.ccafe842.chunk.css"
+  },
+  {
+    "revision": "a138a232de2da99cfea6",
+    "url": "/dvc/static/js/2.3e7f86b1.chunk.js"
+  },
+  {
+    "revision": "7c9cfbb003409cae67a52f3554c1894f",
+    "url": "/dvc/static/js/2.3e7f86b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756885a032312d8a33a5",
+    "url": "/dvc/static/js/3.8465f2c6.chunk.js"
+  },
+  {
+    "revision": "14df406433e67e41ef80",
+    "url": "/dvc/static/js/main.900dd68f.chunk.js"
+  },
+  {
+    "revision": "5c50ae9dd51a34a9a533",
+    "url": "/dvc/static/js/runtime-main.b628ea86.js"
+  },
+  {
+    "revision": "671e427212ac51d36b276e05018688bc",
+    "url": "/dvc/static/media/marca-blanca.671e4272.png"
+  }
+]);
